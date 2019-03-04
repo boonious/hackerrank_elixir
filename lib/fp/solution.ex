cd43f4c0f33f8d71d6ec56edf1bcec01dbf94256
Boon Low <boon.low@mac.com>
2019-03-04 13:39:40 +0000

    evaluating exponential series e^x test and solution

--- lib/fp/solution.ex
+++ lib/fp/solution.ex
@@ -1,11 +1,21 @@
 defmodule Solution.FP do
   @moduledoc """
-  Documentation for Test.
+  Elixir solutions for Hackerrank functional programming challenges.
   """
 
   @doc """
+  Evaluating e^x - https://www.hackerrank.com/challenges/eval-ex/problem
   """
-  def hello do
-    :world
+  @spec exp(float, integer, list) :: float 
+  def exp(x, no_of_terms, results \\ 1)
+  def exp(_x, 0, results), do: results |> Float.round(4)
+  def exp(x, no_of_terms, results) do
+      exp(x, no_of_terms-1, results + nth_term(x, no_of_terms))
   end
+
+  defp nth_term(x, n), do: :math.pow(x,n) / factorial(n)
+  defp factorial(0), do: 1
+  defp factorial(n) when n > 0, do: Enum.reduce(1..n, 1, &*/2)
+
 end
+
--- test/hackerrank_test.exs
+++ test/hackerrank_test.exs
@@ -2,7 +2,15 @@ defmodule SolutionTest do
   use ExUnit.Case
   doctest Solution.FP
 
-  test "greets the world" do
-    assert Solution.FP.hello() == :world
+  # https://www.hackerrank.com/challenges/eval-ex/problem
+  test "exp - Evaluating exponential, e^x" do
+    test_cases = [20.0000, 5.0000, 0.5000, -0.5000]
+    initial_results = 1 # initial a sum to be added to
+    number_of_terms = 9
+
+    test_results = test_cases |> Enum.map(&Solution.FP.exp(&1, number_of_terms, initial_results))
+
+    assert test_results == [2423600.1887, 143.6895, 1.6487, 0.6065]
   end
+
 end
