aebb47ccfa2edf37f98167d9417949dffbba1aad
Boon Low <boon.low@mac.com>
2019-03-18 16:25:34 +0000

    FP data structure challenge: binary tree - simple 3 level balanced tree building

--- lib/fp/structures.ex
+++ lib/fp/structures.ex
@@ -16,5 +16,38 @@ defmodule FP.Structures do
   # functions for adding left, right nodes when leaf is "nil" (empty)
   def add(%{v: v, l: l, r: r} = _n, {lv,rv}) when is_nil(l) and is_nil(r), do: %{v: v, l: n(lv), r: n(rv)}
 
+  # start building tree with a n[1] root, count of 1
+  def build_tree(data), do: build_tree(data, n(1), 1)
+
+  def build_tree([], tree, _count), do: tree
+  def build_tree(data, tree, count) do
+    nodes = data |> Enum.take(count)
+    remaining_nodes = data |> Enum.drop(count)
+
+    # add nodes to tree
+    new_tree = build_tree(nodes, tree)
+
+    # calculate the next batch of nodes in the next level
+    new_count = nodes
+    |> List.flatten
+    |> Enum.filter(&(&1!=-1))
+    |> length
+
+    # build the next level tree nodes
+    build_tree(remaining_nodes, new_tree, new_count)
+  end
+
+  # functions for adding nodes to tree
+  def build_tree([], tree), do: tree
+  def build_tree([n|nodes], tree), do: build_tree(nodes, tree |> add(n))
+
+  # functions for adding left, right nodes alternately when leaf is "nil" (empty)
+  def add(%{v: v, l: l, r: r} = _n, [lv,rv]) when is_nil(l) and is_nil(r), do: %{v: v, l: n(lv), r: n(rv)}
+  def add(%{v: v, l: l, r: r} = _n, value) do
+    cond do
+      l.l == nil -> %{v: v, l: l |> add(value), r: r}
+      r.l == nil -> %{v: v, l: l, r: r |> add(value)}
+    end
+  end
 
 end
--- test/fp_structures_test.exs
+++ test/fp_structures_test.exs
@@ -11,8 +11,18 @@ defmodule FPStructuresTest do
 
     test "n - binary tree node creation / addition" do
       assert n(1) ==  %{l: nil, r: nil, v: 1}
-      assert n(1) |> add({2,3})
+      assert n(1) |> add([2,3])
              == %{v: 1, l: %{l: nil, r: nil, v: 2}, r: %{l: nil, r: nil, v: 3}}
+    test "build_tree - binary tree building" do
+      assert build_tree([[2, 3]]) ==  %{l: nil, r: nil, v: 22}
+      assert build_tree([[2, 3], [-1, -1], [-1, -1]])
+      == %{
+        v: 1,
+        l: %{v: 2, l: %{l: nil, r: nil, v: -1}, r: %{l: nil, r: nil, v: -1}},
+        r: %{v: 3, l: %{l: nil, r: nil, v: -1}, r: %{l: nil, r: nil, v: -1}},
+      }
+    end
+
     end
 
 end
