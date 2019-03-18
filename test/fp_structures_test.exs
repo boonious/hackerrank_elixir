602c23630c8ede92828544eeee674ff1634bd9cd
Boon Low <boon.low@mac.com>
2019-03-18 16:29:14 +0000

    fix broken test

--- test/fp_structures_test.exs
+++ test/fp_structures_test.exs
@@ -7,14 +7,13 @@ defmodule FPStructuresTest do
   describe "structures" do
     @describetag :structures
 
-  end
-
     test "n - binary tree node creation / addition" do
       assert n(1) ==  %{l: nil, r: nil, v: 1}
-      assert n(1) |> add([2,3])
-             == %{v: 1, l: %{l: nil, r: nil, v: 2}, r: %{l: nil, r: nil, v: 3}}
+      assert n(1) |> add([2,3]) == %{v: 1, l: %{l: nil, r: nil, v: 2}, r: %{l: nil, r: nil, v: 3}}
+    end
+
     test "build_tree - binary tree building" do
-      assert build_tree([[2, 3]]) ==  %{l: nil, r: nil, v: 22}
+      assert build_tree([[2, 3]]) ==  %{l: %{l: nil, r: nil, v: 2}, r: %{l: nil, r: nil, v: 3}, v: 1}
       assert build_tree([[2, 3], [-1, -1], [-1, -1]])
       == %{
         v: 1,
