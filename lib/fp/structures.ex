676e8182f48f3daa5d93882236e9584ea98e70c3
Boon Low <boon.low@mac.com>
2019-03-18 11:12:57 +0000

    FP data structure challenge: binary tree, defines a basic tree node and addition function for experimentation

--- lib/fp/structures.ex
+++ lib/fp/structures.ex
@@ -3,4 +3,18 @@ defmodule FP.Structures do
   Elixir solutions for HackerRank functional programming challenges: functional structures.
   """
 
+  #==========================================================================
+  @doc """
+  Binary tree: swap and traverse nodes
+
+  https://www.hackerrank.com/challenges/swap-nodes/problem
+  """
+
+  # define a binary tree node of value (v) with left (l), right (r) leaves
+  def n(value), do: %{v: value, l: nil, r: nil}
+
+  # methods for adding nodes
+  def add(%{v: v, l: _l, r: r} = _n, value, :left), do: %{v: v, l: n(value), r: r}
+  def add(%{v: v, l: l, r: _r} = _n, value, :right), do: %{v: v, l: l, r: n(value)}
+
 end
