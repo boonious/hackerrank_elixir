f65af9f05df193f77096cf4fe8ffecec44677929
Boon Low <boon.low@mac.com>
2019-03-21 08:48:20 +0000

    FP structures challenge: swapping nodes on binary tree, see “FIXME” for test case > 1000 nodes

--- lib/fp/structures.ex
+++ lib/fp/structures.ex
@@ -13,6 +13,30 @@ defmodule FP.Structures do
   # define a binary tree node of value (v) with left (l), right (r) leaves
   def n(value), do: %{v: value, l: nil, r: nil}
 
+  def swap_nodes(data) do
+    [[n] | x ] = data
+
+    nodes_data = Enum.take(x, n)
+    [_ | swap_data] = Enum.drop(x, n) |> List.flatten
+
+    # build the binary tree
+    {depth, tree} = build_tree(nodes_data)
+
+    output = []
+    swap_nodes(tree, swap_data, depth, output)
+  end
+
+  def swap_nodes(_, [], _, output), do: output |> Enum.reverse
+  def swap_nodes(tree, swap_data, depth, output) when is_list(swap_data) do
+    level = hd swap_data
+
+    k = depth_series(level, 1, [level], depth) |> Enum.reverse
+    x = swap(tree, 1, depth, k)
+    y = x |> inorder_traverse
+
+    swap_nodes(x, swap_data |> tl, depth, [y|output])
+  end
+
   def swap(tree, current_depth, depth, _k) when current_depth == depth, do: tree
   def swap(%{l: nil, r: nil, v: -1}, _, _, _), do: %{l: nil, r: nil, v: -1}
   def swap(%{v: v, l: l, r: r} = _tree, current_depth, depth, k) do
@@ -25,6 +49,8 @@ defmodule FP.Structures do
     end
   end
 
+  # implement the algorithm below for Elixir
+  # https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion
   def inorder_traverse(tree) do
     {stack, output} = {[], []}
 
@@ -136,13 +162,13 @@ defmodule FP.Structures do
   # for adding the next child node pair to
   #
   # FIXME: this involves recursion into nested depth nodes, 
-  # as the calling "add" is alreading recursing, this is
+  # as the calling "add" is already recursing, this is
   # not performant for very deep large tree, 
-  # e.g the last 2 HackerRank test case for tree with nodes > 1000 nodes
+  # e.g the last 2 HackerRank test cases for tree with many nodes > 1000 nodes
   #
-  # The algorithm to fix this I'm currently thinking: pre-storing (single depth) 
-  # nodes in a stack while adding them, so that these can be used for
-  # the next depth level node building. This eliminates this recursive function.
+  # The algorithm to fix this, I'm currently thinking: pre-storing (single level)
+  # nodes in a stack while adding them, so that these can be matched for
+  # the next depth level node building. This eliminates the deep recursive function below.
   defp nil_leaf?(node, depth, _side) do
     leaves = leaf_nodes([node], depth) |> Enum.find(fn x -> x.l == nil and x.v != -1 end)
     if (leaves != nil), do: true, else: false
@@ -154,6 +180,11 @@ defmodule FP.Structures do
     leaf_nodes(x, depth - 1)
   end
 
+  # return levels in 1k,2k,3k.. for a given first swap 'k' level
+  defp depth_series(k, nth, [l|levels], depth) when l < depth - 1 do
+    x = [l|levels]
+    depth_series(k, nth+1, [(nth+1)*k|x], depth)
   end
+  defp depth_series(_k, _nth, levels, depth), do: levels |> Enum.reject(&(&1 > depth - 1))
 
 end
