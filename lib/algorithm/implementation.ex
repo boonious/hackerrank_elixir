da8616be9c8a8f62273ddf8270d956d4ac84c203
Boon Low <boon.low@mac.com>
2019-04-08 11:49:18 +0100

    begin algorithm challenges: repeated string implementation

---
+++ lib/algorithm/implementation.ex
@@ -0,0 +1,15 @@
+defmodule Algo.Imp do
+  @moduledoc """
+  Elixir solutions for HackerRank algorithm challenges: implementation.
+  """
+
+  #==========================================================================
+  @doc """
+  Repeated string
+
+  https://www.hackerrank.com/challenges/repeated-string/problem
+  """
+  def repeated_string(str, length) do
+  end
+
+end
---
+++ test/algorithm/implementation_test.exs
@@ -0,0 +1,16 @@
+defmodule AlgoImpTest do
+  use ExUnit.Case
+  import Algo.Imp
+
+  doctest Algo.Imp
+
+  describe "algorithm - implementation" do
+    @describetag :implementation
+
+    # https://www.hackerrank.com/challenges/repeated-string/problem
+    test "repeated_string - Repeated string" do
+    end
+
+  end
+
+end
